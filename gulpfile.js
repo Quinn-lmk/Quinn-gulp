536509d64af15393af3cfd5a2200c5207d9ec87c
limingkun <445635121@qq.com>
2016-08-23 02:18:37 +0800

    launch chrome second

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ livereload = require('gulp-livereload'),  //本地更改刷新服务器
 imagemin   = require('gulp-imagemin'),    
 pngquant   = require('imagemin-pngquant'),  //压缩图片的第一个插件
 tinypng    = require('gulp-tinypng'),      //压缩图片2，国外的，更好用
-zip        = require('gulp-zip'),         //给app打包，一般使用git
+zip        = require('gulp-zip');        //给app打包，一般使用git
 
 
 // 检查 js，Link任务会检查 js/ 目录下得js文件有没有报错或警告。
@@ -29,8 +29,48 @@ gulp.task('lint', function(cb) {
     gulp.src('./test/js/*.js')
         .pipe(jshint())   
         .pipe(jshint.reporter('default'));
-    cb(err);
+    cb();
 });
+// 编译Sass，Sass任务会编译 scss/ 目录下的scss文件，并把编译完成的css文件保存到 /css 目录中。
+gulp.task('sass', function() {
+    gulp.src('./test/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./test/css'));
+});
+
+//开启本地 Web,用哪个时配置哪个，config是前面引入自己定义的json文件
+//livereload 可以自动刷新浏览器
+gulp.task('webserver', function(cb) {
+  gulp.src( './' )
+    .pipe(webserver({
+      host:             config.localserver.host,
+      port:             config.localserver.port,
+      livereload:       true,
+      directoryListing: false
+    }));
+    cb();
+});
+//通过浏览器打开本地 Web服务器 路径
+gulp.task('openbrowser',function() {
+  opn( 'http://' + config.localserver.host + ':' + config.localserver.port +"/" + config.testHtmlAddress);
+});
+
+//一但修改就在配置的网页中刷新 ot=ontime
+//检查js,编译css
+gulp.task('default',function(){
+    gulp.run('webserver'); //开启服务器
+    gulp.watch(['./test/scss/*.scss','./test/*.html','./test/js/*.js'],function(){
+        gulp.run('openbrowser',['lint','sass'],function(){
+            console.log('========success========')
+        }); //打开服务器前先检查js,编译css
+    });
+});
+
+//压缩JS，html,css,图片 生成项目目录
+
+
+
 
 // 合并，压缩 JS 
 // 中间加了lint,表示必须检查脚本之后运行
@@ -41,78 +81,6 @@ gulp.task('scripts',['lint'], function() {
         .pipe(uglify())
         .pipe(gulp.dest('./app/js'));
 });
-
-// 编译Sass，Sass任务会编译 scss/ 目录下的scss文件，并把编译完成的css文件保存到 /css 目录中。
-gulp.task('sass', function() {
-    gulp.src('./test/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./test/css'));
-});
-
-
-
-gulp.task('scss', () => {
-  gulp
-    .src('./test/scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./test/who'))
-    .pipe(refresh())
-})
- 
-// gulp.task('watch', () => {
-//   refresh.listen()
-//   gulp.watch('./test/scss/*.scss', ['scss'])
-// })
-gulp.task('lmk',function(){
-    gulp.run('webserver');
-    gulp.watch('./test/scss/*.scss',function(){
-        gulp.run('o');
-    });
-});
-
-
-
-//开启本地 Web,用哪个时配置哪个，config是前面引入自己定义的json文件
-//livereload 可以自动刷新浏览器
-gulp.task('webserver', function(cb) {
-  gulp.src( './' )
-    .pipe(webserver({
-      host:             "localhost",
-      port:             "8081",
-      livereload:       true,
-      directoryListing: false
-    }));
-    cb();
-});
-//通过浏览器打开本地 Web服务器 路径
-gulp.task('openbrowser',function() {
-  opn( 'http://' + config.localserver.host + ':' + config.localserver.port );
-});
-
-//默认任务
-gulp.task('o', function(){
-  gulp.run('openbrowser');
-});
-
-
-// // 默认任务，使用 .run() 方法关联和运行我们上面定义的任务
-// gulp.task('default', function(){
-//     gulp.run('lint', 'sass', 'scripts');
-
-//     // 监听文件变化
-//     gulp.watch('./js/*.js', function(){
-//         gulp.run('lint', 'sass', 'scripts');
-//     });
-//     gulp.watch('./scss/*.scss', function(){
-//         gulp.run('lint', 'sass', 'scripts');
-//     });
-
-// });
-
-
-
-//下面是后期考虑的
 
 //压缩html
 gulp.task('minihtml', function() {
